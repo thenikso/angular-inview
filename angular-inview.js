e9eb084d129e0d195349662a56fc2a07a7d92bbe
Nicola Peduzzi <thenikso@gmail.com>
2014-12-03 13:59:30 +0100

    Add quick FRP utilities

--- angular-inview.js
+++ angular-inview.js
@@ -63,4 +63,134 @@ function getViewportSize () {
   return result;
 }
 
+// ## QuickSignal FRP
+// A quick and dirty implementation of Rx to have a streamlined code in the
+// directives.
+
+// ### QuickSignal
+//
+// - `didSubscribeFunc`: a function receiving a `subscriber` as described below
+//
+// Usage:
+//     var mySignal = new QuickSignal(function(subscriber) { ... })
+function QuickSignal (didSubscribeFunc) {
+  this.didSubscribeFunc = didSubscribeFunc;
+}
+
+// Subscribe to a signal and consume the steam of data.
+//
+// Returns a function that can be called to stop the signal stream of data and
+// perform cleanup.
+//
+// A `subscriber` is a function that will be called when a new value arrives.
+// a `subscriber.$dispose` property can be set to a function to be called uppon
+// disposal. When setting the `$dispose` function, the previously set function
+// should be chained.
+QuickSignal.prototype.subscribe = function (subscriber) {
+  this.didSubscribeFunc(subscriber);
+  var dispose = function () {
+    if (subscriber.$dispose) {
+      subscriber.$dispose();
+      subscriber.$dispose = null;
+    }
+  }
+  return dispose;
+}
+
+QuickSignal.prototype.map = function (f) {
+  var s = this;
+  return new QuickSignal(function (subscriber) {
+    subscriber.$dispose = s.subscribe(function (nextValue) {
+      subscriber(f(nextValue));
+    });
+  });
+};
+
+QuickSignal.prototype.filter = function (f) {
+  var s = this;
+  return new QuickSignal(function (subscriber) {
+    subscriber.$dispose = s.subscribe(function (nextValue) {
+      if (f(nextValue)) {
+        subscriber(nextValue);
+      }
+    });
+  });
+};
+
+QuickSignal.prototype.scan = function (initial, scanFunc) {
+  var s = this;
+  return new QuickSignal(function (subscriber) {
+    var last = initial;
+    subscriber.$dispose = s.subscribe(function (nextValue) {
+      last = scanFunc(last, nextValue);
+      subscriber(last);
+    });
+  });
+}
+
+QuickSignal.prototype.merge = function (signal) {
+  return signalMerge(this, signal);
+};
+
+QuickSignal.prototype.throttle = function (threshhold) {
+  var s = this, last, deferTimer;
+  return new QuickSignal(function (subscriber) {
+    var chainDisposable = s.subscribe(function () {
+      var now = +new Date,
+          args = arguments;
+      if (last && now < last + threshhold) {
+        clearTimeout(deferTimer);
+        deferTimer = setTimeout(function () {
+          last = now;
+          subscriber.apply(null, args);
+        }, threshhold);
+      } else {
+        last = now;
+        subscriber.apply(null, args);
+      }
+    });
+    subscriber.$dispose = function () {
+      clearTimeout(deferTimer);
+      if (chainDisposable) chainDisposable();
+    };
+  });
+};
+
+function signalMerge () {
+  var signals = arguments;
+  return new QuickSignal(function (subscriber) {
+    var disposables = [];
+    for (var i = signals.length - 1; i >= 0; i--) {
+      disposables.push(signals[i].subscribe(function () {
+        subscriber.apply(null, arguments);
+      }));
+    }
+    subscriber.$dispose = function () {
+      for (var i = disposables.length - 1; i >= 0; i--) {
+        if (disposables[i]) disposables[i]();
+      }
+    }
+  });
+}
+
+// Returns a signal from DOM events of a target.
+function signalFromEvent (target, event) {
+  return new QuickSignal(function (subscriber) {
+    var handler = function (e) {
+      subscriber(e);
+    };
+    var el = angular.element(target);
+    el.bind(event, handler);
+    subscriber.$dispose = function () {
+      el.unbind(event, handler);
+    };
+  });
+}
+
+function signalSingle (value) {
+  return new QuickSignal(function (subscriber) {
+    subscriber(value);
+  });
+}
+
 })();
