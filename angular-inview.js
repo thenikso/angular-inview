17d91e528175b168761d0adc326d0680bc768b41
Nicola Peduzzi <thenikso@gmail.com>
2016-08-18 13:30:07 +0200

    Added more comprehensive list of events to bind to. See ce9d005f9d3df4fb254d281acae6fc3ebae9c69b by @dganoff

--- angular-inview.js
+++ angular-inview.js
@@ -62,7 +62,7 @@ function inViewDirective ($parse) {
       var viewportEventSignal = signalSingle({ type: 'initial' })
 
       // Merged with the window events
-      .merge(signalFromEvent(window, 'scroll resize'))
+      .merge(signalFromEvent(window, 'checkInView click ready wheel mousewheel DomMouseScroll MozMousePixelScroll resize scroll touchmove mouseup'))
 
       // Merge with container's events signal
       if (container) {
