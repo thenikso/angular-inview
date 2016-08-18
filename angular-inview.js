fb2b61379053da5193575a73e15caef07907c6c4
Nicola Peduzzi <thenikso@gmail.com>
2016-08-18 10:40:46 +0200

    Update version in the docs

--- angular-inview.js
+++ angular-inview.js
@@ -1,8 +1,8 @@
 // # Angular-Inview
 // - Author: [Nicola Peduzzi](https://github.com/thenikso)
 // - Repository: https://github.com/thenikso/angular-inview
-// - Install with: `bower install angular-inview`
-// - Version: **2.0-alpha1**
+// - Install with: `npm install angular-inview@beta`
+// - Version: **2.0.0-beta.1**
 (function() {
 'use strict';
 
